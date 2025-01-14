9d7b56dfd12a33232bcaae5914b7c262828316e7
Abiola Olamilekan <abiolawalter2015@gmail.com>
2025-01-14 01:54:31 +0100

    feat: implement landing page and clerk authentication

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,9 @@
+type Props = {
+  children: React.ReactNode
+}
+
+export default function AuthLayout({ children }: Props) {
+  return (
+    <div className="h-screen flex items-center justify-center">{children}</div>
+  )
+}
