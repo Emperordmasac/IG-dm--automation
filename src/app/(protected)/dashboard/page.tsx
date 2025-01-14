df2fb8fe5d0d0b58cba7e97a8dbf7930856af6e1
Abiola Olamilekan <abiolawalter2015@gmail.com>
2025-01-14 11:10:04 +0100

    create prisma schema for the app

---
+++ src/app/(protected)/dashboard/page.tsx
@@ -0,0 +1,9 @@
+type Props = {}
+
+const DashboardPage = (props: Props) => {
+  // server action to onboard the user
+
+  return <div></div>
+}
+
+export default DashboardPage
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,9 @@ import { Inter } from "next/font/google"
 import { ClerkProvider } from "@clerk/nextjs"
 import "./globals.css"
 
+import { Toaster } from "sonner"
+import { ThemeProvider } from "@/providers/theme-provider"
+
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
@@ -19,7 +22,17 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body suppressHydrationWarning className={inter.className}>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+            <Toaster />
+          </ThemeProvider>
+        </body>
       </html>
     </ClerkProvider>
   )
